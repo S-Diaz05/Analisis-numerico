cc3f1e2433349a98548f4c9cf18b8eabf0ed4f02
Santiago Diaz <61949658+S-Diaz05@users.noreply.github.com>
2021-02-24 00:10:25 -0500

    Add files via upload

---
+++ PosicionFalsa.py
@@ -0,0 +1,23 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30907.101
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{888888A0-9F3D-457C-B088-3A5042F75D52}") = "PosicionFalsa", "PosicionFalsa\PosicionFalsa.pyproj", "{5CBCF7D9-7E45-4B26-8233-6091DBAEA4CC}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{5CBCF7D9-7E45-4B26-8233-6091DBAEA4CC}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{5CBCF7D9-7E45-4B26-8233-6091DBAEA4CC}.Release|Any CPU.ActiveCfg = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {FD2980EE-E083-410E-A585-09CF4E98D0A9}
+	EndGlobalSection
+EndGlobal
